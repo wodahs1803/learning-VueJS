5439bdeb86f633f76e1a5a28a83243bc23b402ca
wodahs1803 <flaviofilho180399@gmail.com>
2022-01-13 23:12:03 -0300

    v0.6 - VueJS Project version

---
+++ vuejs-cli/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+import Comments from './components/Comments.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Comments
+  }
+}
+</script>
+
+<style>
+
+</style>
